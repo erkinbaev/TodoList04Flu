e289eee529650e1ca714c00e32c51a7a0d258fdd
Nursultan <nursultan@MacBook-Air-Nursultan.local>
2026-04-09 00:33:31 +0600

    initial commit

---
+++ lib/add/add_page.dart
@@ -0,0 +1,85 @@
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class AddPage extends StatefulWidget{
+  const AddPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AddPage();
+}
+
+class _AddPage extends State<AddPage> {
+  late Timer _timer;
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("AddPage - initState");
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      final date = DateTime.now();
+      print("${date.minute}: ${date.second}");
+    });
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("AddPage - didChangeDependencies");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("AddPage - build");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Добавить задачу"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            TextField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(), 
+                label: Text("Введите название задачи")
+                )
+              ),
+            TextButton(onPressed: () => _saveTodo(), child: Text("Сохранить"))
+          ],
+        ),
+      )
+    );
+  }
+
+  void _saveTodo() {
+    Navigator.pop(context, _textEditingController.text);
+  }
+
+  @override
+  void didUpdateWidget(covariant AddPage oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("AddPage - didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print("AddPage - deactivate");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("AddPage - dispose");
+    _timer.cancel();
+  }
+}
---
+++ lib/home/home_page.dart
@@ -0,0 +1,91 @@
+
+import 'package:flutter/material.dart';
+import 'package:todo_list_04flu/add/add_page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("HomePage - initState");
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    print("HomePage - didChangeDependencies");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("HomePage - build");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _navigateToAddPage,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future _navigateToAddPage() async {
+   final result = await Navigator.push(context, MaterialPageRoute(builder: (_) => const AddPage()));
+   if (result != null) {
+    print("Текст со второго экрана: $result");
+   }
+  }
+
+  @override
+  void didUpdateWidget(covariant MyHomePage oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    print("HomePage - didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    // TODO: implement deactivate
+    super.deactivate();
+    print("HomePage - deactivate");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print("HomePage - dispose");
+  }
+}
